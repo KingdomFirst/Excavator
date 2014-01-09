4165dfa9ffd48d44b47fa9aaa1184409f4635cc1
unknown <david.stevens@CEN-DSTEVENS.ad.newspring.cc>
2014-01-09 17:38:29 -0500

    Added F1 excavator (extends base class)

---
+++ F1/F1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Conversion.F1
+{
+    class F1 : Excavator
+    {
+    }
+}
