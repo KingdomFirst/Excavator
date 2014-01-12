28f486f791ea1f92241b50b1a99ee58d72f68592
David Stevens <david.stevens@newspring.cc>
2014-01-12 17:04:40 -0500

    Update styling changes to match check scanning app

--- Program.Designer.cs
+++ Program.Designer.cs
@@ -33,12 +33,15 @@
             // 
             // Program
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MinimumSize = new System.Drawing.Size(550, 350);
             this.Name = "Program";
             this.Text = "Rock Excavator v.1";
             this.Load += new System.EventHandler(this.Program_Load);
