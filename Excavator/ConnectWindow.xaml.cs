f2b2b66ba8190c4aefbb7f36df531e66e957b83d
David Stevens <david.stevens@newspring.cc>
2014-01-22 12:16:26 -0500

    Project reorg, still need to get windows loading inside the main nav

---
+++ Excavator/ConnectWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Excavator
+{
+    /// <summary>
+    /// Interaction logic for ConnectWindow.xaml
+    /// </summary>
+    public partial class ConnectWindow : Window
+    {
+        public ConnectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
