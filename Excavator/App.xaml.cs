78d51a41732a918afa4238029c460adca05b9e29
David Stevens <david.stevens@newspring.cc>
2014-06-18 10:31:28 -0400

    Remove error handling since it needs to be merged from feature-csv

--- Excavator/App.xaml.cs
+++ Excavator/App.xaml.cs
@@ -49,21 +49,6 @@ namespace Excavator
                 }
             };
 
-            app.Dispatcher.UnhandledException += ( sender, ex ) =>
-            {
-                var appLog = new System.Diagnostics.EventLog();
-                appLog.Source = "Excavator";
-                appLog.WriteEntry( ex.ToString(), System.Diagnostics.EventLogEntryType.Error );
-                ex.Handled = true;
-            };
-
-            AppDomain.CurrentDomain.UnhandledException += ( s, ex ) =>
-            {
-                var appLog = new System.Diagnostics.EventLog();
-                appLog.Source = "Excavator";
-                appLog.WriteEntry( ex.ToString(), System.Diagnostics.EventLogEntryType.Error );
-            };
-
             app.InitializeComponent();
 
             app.Run();
