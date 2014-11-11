8ec86ddb346fb045894b56b7d7d803fc3ca9a96e
dcs619 <dcs619@gmail.com>
2014-11-11 16:42:46 -0500

    Validate email addresses, fix #80

--- Excavator.CSV/Maps/Individual.cs
+++ Excavator.CSV/Maps/Individual.cs
@@ -509,7 +509,7 @@ namespace Excavator.CSV
                     }
 
                     string primaryEmail = row[Email];
-                    if ( !string.IsNullOrWhiteSpace( primaryEmail ) )
+                    if ( !string.IsNullOrWhiteSpace( primaryEmail ) && primaryEmail.IsValidEmail() )
                     {
                         person.Email = primaryEmail;
                         person.IsEmailActive = isEmailActive;
@@ -517,7 +517,7 @@ namespace Excavator.CSV
                     }
 
                     string secondaryEmailValue = row[SecondaryEmail];
-                    if ( !string.IsNullOrWhiteSpace( secondaryEmailValue ) )
+                    if ( !string.IsNullOrWhiteSpace( secondaryEmailValue ) && secondaryEmailValue.IsValidEmail() )
                     {
                         AddPersonAttribute( secondaryEmailAttribute, person, secondaryEmailValue );
                     }
