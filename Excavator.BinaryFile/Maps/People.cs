0c45dc14d0876eaf07309a7a4e3617d78f9d177d
dcs619 <dcs619@gmail.com>
2015-08-13 11:19:04 -0400

    added people import

---
+++ Excavator.BinaryFile/Maps/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excavator.BinaryFile
+{
+    /// <summary>
+    /// Partial of BinaryFile import that holds a Person map
+    /// </summary>
+    public partial class BinaryFileComponent
+    {
+        public void MapPeople( Dictionary<string, string> settings )
+        {
+            //var storageLocation = FileTypes.
+        }
+    }
+}
